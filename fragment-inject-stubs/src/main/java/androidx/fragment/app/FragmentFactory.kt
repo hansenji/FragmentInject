73dd8a56aff07504d726891233695aadb074e4de
Jordan Hansen <jordanihansen10@gmail.com>
2020-02-03 14:42:44 -0700

    Fixed Missin dependencies in Stubs

--- fragment-inject-stubs/src/main/java/androidx/fragment/app/FragmentFactory.kt
+++ fragment-inject-stubs/src/main/java/androidx/fragment/app/FragmentFactory.kt
@@ -1,7 +1,5 @@
 package androidx.fragment.app
 
-import androidx.fragment.app.Fragment
-
 abstract class FragmentFactory() {
 
     abstract fun instantiate(classLoader: ClassLoader, className: String): Fragment
